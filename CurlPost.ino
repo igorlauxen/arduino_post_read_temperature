6de22245880550d28791c609c4a7b2da6f3ebeca
i841640 <igor.lauxen@sap.com>
2017-05-23 14:41:58 -0300

    temperature sensor

--- CurlPost.ino
+++ CurlPost.ino
@@ -5,8 +5,7 @@
 String INUMBER_TRIAL = "i841640"; 
 String DEVICE_ID = "32b9f043-8569-464e-8bc4-95c567d98d00";
 String MESSAGE_TYPE = "e0a6c3bab615e97310f0";
-//dummy entries
-int CALOR_TEST = 130;
+float CALOR_TEST = 130;
 String URL_TO_SERVICE = "";
 String USER_AUTHENTICATION = "Bearer YOUR_DEVICE_TOKEN";
 
@@ -47,8 +46,23 @@ void setup(){
   Serial.println("Starting...");
 }
 
+/*
+BLACK = GND
+RED = VCC 5V
+YELLOW = Pin 7 Analog
+
+*/
+
 void loop(){
-  CALOR_TEST = random(1,20);
+  CALOR_TEST = getTemperatureInCelsus();
   URL_TO_SERVICE = "https://iotmms"+INUMBER_TRIAL+"trial.hanatrial.ondemand.com/com.sap.iotservices.mms/v1/api/http/data/"+DEVICE_ID+"/"+MESSAGE_TYPE+"?test="+CALOR_TEST;
   post();
+}
+
+//https://www.arduino.cc/en/Tutorial/TemperatureWebPanel
+float getTemperatureInCelsus(){
+  double val = analogRead(7);
+  float voltage = val * (5000.0f / 1024.0f);
+  float temperatureC = (voltage - 500.0f) / 10.0f;
+  return temperatureC;
 }
