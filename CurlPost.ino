bafc89dee6d65656e6d96c34457c8acc0a85a1e4
i841640 <igor.lauxen@sap.com>
2017-05-23 13:17:02 -0300

    Removed unnecessary things

--- CurlPost.ino
+++ CurlPost.ino
@@ -1,22 +1,4 @@
 #include <Bridge.h>
-#include <BridgeClient.h>
-#include <BridgeServer.h>
-#include <BridgeSSLClient.h>
-#include <BridgeUdp.h>
-#include <Console.h>
-#include <FileIO.h>
-#include <HttpClient.h>
-#include <Mailbox.h>
-#include <Process.h>
-#include <YunClient.h>
-#include <YunServer.h>
-
-/*
-
-Tests over tests
-
-*/
-
 #include <Process.h>
 
 //services setups     
@@ -25,23 +7,14 @@ String DEVICE_ID = "32b9f043-8569-464e-8bc4-95c567d98d00";
 String MESSAGE_TYPE = "e0a6c3bab615e97310f0";
 //dummy entries
 int CALOR_TEST = 130;
-String SENDER = "Arduino Yun";
-String URL_TO_SERVICE = "https://iotmms"+INUMBER_TRIAL+"trial.hanatrial.ondemand.com/com.sap.iotservices.mms/v1/api/http/data/"+DEVICE_ID+"/"+MESSAGE_TYPE+"?test="+CALOR_TEST;
-
-//basic 64 -- use postman to generate it
-//String USER_AUTHENTICATION = "Basic aTg0MTY0MDpRYVJ1bGV6MjJA";
+String URL_TO_SERVICE = "";
 String USER_AUTHENTICATION = "Bearer b46b9a15c5c0e3e517c9f516e925723";
 
+// The SAP proxy is handled in the file /etc/profile
+//export http_proxy=http://proxy.wdf.sap.corp:8080
+//export https_proxy=http://proxy.wdf.sap.corp:8080 
 
-
-// Everytime you reset your arduino, you need to reset your proxy again
-/*
-export http_proxy=http://proxy.wdf.sap.corp:8080
-export https_proxy=http://proxy.wdf.sap.corp:8080 
-*/
-
-
-String createCurlCommand (String url, int calor, String sender, String authentication, String messageType) {
+String createCurlCommand (String url, int calor, String authentication, String messageType) {
     
   String curlCmd = "curl ";
   //header parameters
@@ -60,40 +33,12 @@ String createCurlCommand (String url, int calor, String sender, String authentic
 }
 
 void post(){
-  String curlCmd = createCurlCommand( URL_TO_SERVICE, CALOR_TEST, SENDER, USER_AUTHENTICATION, MESSAGE_TYPE );
+  String curlCmd = createCurlCommand( URL_TO_SERVICE, CALOR_TEST, USER_AUTHENTICATION, MESSAGE_TYPE );
   Serial.println("Curl Cmd Command being saved: ");
   Serial.println(curlCmd);
   Process process;
   process.runShellCommandAsynchronously(curlCmd);
-
-  while (process.running()){
-    while (process.available()>0) {
-      char c = process.read();
-      //Serial.println(c);
-    }
-    Serial.println("\n");
-    Serial.println("########################################");
-  }
-}
-
-void remove_http_proxy(){
-  String remove_http = "export http_proxy=http://proxy.wdf.sap.corp:8080";
-  remove_proxy(remove_http);
-}
-
-
-void remove_https_proxy(){
-  String remove_https = "export https_proxy=http://proxy.wdf.sap.corp:8080";
-  remove_proxy(remove_https);
-}
-
-void remove_proxy(String proxyCommand){
-  Process p;
-  p.runShellCommand(proxyCommand);
-  // do nothing until the process finishes
-  while (p.running());
-  Serial.println("Proxy Removed: "+ proxyCommand);
-  
+  while (process.running());
 }
 
 void setup(){
@@ -103,8 +48,6 @@ void setup(){
 }
 
 void loop(){
-    remove_http_proxy();
-    remove_https_proxy();
     CALOR_TEST = random(1,20);
     URL_TO_SERVICE = "https://iotmms"+INUMBER_TRIAL+"trial.hanatrial.ondemand.com/com.sap.iotservices.mms/v1/api/http/data/"+DEVICE_ID+"/"+MESSAGE_TYPE+"?test="+CALOR_TEST;
     post();
